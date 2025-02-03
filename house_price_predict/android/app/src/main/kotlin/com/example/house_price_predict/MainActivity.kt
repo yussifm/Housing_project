bf5a707f05c90fec3802020a32a2047c7ecb9f44
Mohammed Yussif <mohammed.yussif@invennico.com>
2025-02-03 17:39:08 +0000

    adding flutter project

--- house_price_predict/android/app/src/main/kotlin/com/example/house_price_predict/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.house_price_predict
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ house_price_predict_flutter_app/android/app/src/main/kotlin/com/example/house_price_predict_flutter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.house_price_predict_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
